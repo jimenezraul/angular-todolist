58306c533c315f760d9732292e98af45eac4c7df
Raul <jimenezraul1981@gmail.com>
2025-01-17 09:24:12 -0500

    add modal component for adding todos; implement modal toggle functionality in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,18 +3,24 @@ import { ContainerComponent } from "../../shared/container/container.component";
 import { TodosComponent } from "../../shared/todos/todos.component";
 import { type Todos, todos } from '../../shared/todos/todo.model';
 import { TodoDetailComponent } from "../../shared/todo-detail/todo-detail.component";
+import { ModalComponent } from "../../shared/modal/modal.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ContainerComponent, TodosComponent, TodoDetailComponent],
+  imports: [ContainerComponent, TodosComponent, TodoDetailComponent, ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  openModal = false;
   selectedTodo: Todos | undefined;
   @Input() todoList: Todos[] = todos
 
   onSelected(id: string) {
     this.selectedTodo = this.todoList.find((todo) => todo.id === id)
+  }
+
+  onModalOpenOrClose() {
+    this.openModal = !this.openModal
   }
 }
